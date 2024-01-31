24822f398e75d8275ad0eee4406b26bd85ad1cf6
macjjuni <macjjuni@gmail.com>
2024-01-31 14:04:51 +0900

    feat. style.ts 타입 에러 수정

--- src/common/util/style.ts
+++ src/common/util/style.ts
@@ -1,11 +1,9 @@
 import { CSSProperties } from 'react';
 
-const setStyleElement = (element: HTMLElement, attrs: CSSProperties): void => {
-  if (attrs !== undefined) {
-    Object.keys(attrs).forEach((key: keyof attrs) => {
-      element.style[key] = attrs[key];
-    });
-  }
+const setStyleElement = (element: HTMLElement, style: CSSProperties): void => {
+  Object.entries(style).forEach(([property, value]) => {
+    element.style.setProperty(property, value as string);
+  });
 };
 
 export default { setStyleElement };
