f41e7f11ea580ab8f80835c61e7b97fd1b2c6e31
macjjuni <macjjuni@gmail.com>
2024-02-04 23:49:16 +0900

    design. 스토리북 컴포넌트 디자인 수정

--- stories/components/common/Container.tsx
+++ stories/components/common/Container.tsx
@@ -1,14 +1,18 @@
 import {CSSProperties, ReactNode} from 'react';
 
-
 const containerStyle: CSSProperties = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
-    gap: '32px',
-    padding: '1.5rem',
     borderRadius: '8px',
     border: '1px solid #d1d1d1',
+};
+
+const darkContainerStyle: CSSProperties = {
+    ...containerStyle,
+    background: '#212121',
+    color: '#fff',
+    marginTop: '16px',
 };
 
 export const Container = ({children}: { children: ReactNode }) => (
@@ -17,6 +21,11 @@ export const Container = ({children}: { children: ReactNode }) => (
     </div>
 );
 
+export const DarkContainer = ({children}: { children: ReactNode }) => (
+    <div style={darkContainerStyle}>
+        {children}
+    </div>
+);
 
 const titleStyle: CSSProperties = {
     display: 'block',
@@ -26,8 +35,19 @@ const titleStyle: CSSProperties = {
     padding:'0 0 16px 0',
     borderBottom: '1px solid #d1d1d1',
 }
+
+const defaultItemStyle: CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    gap: '20px',
+    width: '100%',
+    padding: '16px'
+}
+
+
 export const Item = ({label, children, className}: { label: string; children: ReactNode, className?: string }) => (
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', gap: '20px', width: '100%'}}
+    <div style={defaultItemStyle}
         className={className}>
         <h2 style={titleStyle}>{label}</h2>
         <div style={{display: 'flex', alignItems: 'center', gap: '24px', flexWrap: 'wrap'}}>
