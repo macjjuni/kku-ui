b13157fe67463c9720242aa380cf15dfe894d3ba
macjjuni <macjjuni@gmail.com>
2024-02-23 00:14:10 +0900

    feature. 버튼 스토리북 개편

--- stories/components/button/KButton.stories.tsx
+++ stories/components/button/KButton.stories.tsx
@@ -1,8 +1,6 @@
 import type {Meta, StoryObj} from '@storybook/react';
-import {Container, Item} from '../common/Container';
-import {KButton, KButtonRefs} from '@/components';
+import {KButton} from '@/components';
 import {KButtonProps} from '@/components/button/KButton.interface';
-import {useEffect, useRef} from 'react';
 
 const meta: Meta<KButtonProps> = {
     component: KButton,
@@ -15,70 +13,16 @@ type Story = StoryObj<typeof KButton>
 
 const Template = (args: KButtonProps) => {
 
-    const loadButtonRef1 = useRef<KButtonRefs>(null);
-    const loadButtonRef2 = useRef<KButtonRefs>(null);
-    const loadButtonRef3 = useRef<KButtonRefs>(null);
-    const loadButtonRef4 = useRef<KButtonRefs>(null);
-    const loadButtonRef5 = useRef<KButtonRefs>(null);
 
-    useEffect(() => {
-        loadButtonRef1.current!.startLoading();
-        loadButtonRef2.current!.startLoading();
-        loadButtonRef3.current!.startLoading();
-        loadButtonRef4.current!.startLoading();
-        loadButtonRef5.current!.startLoading();
-    }, []);
-
-    return (
-        <Container>
-            <Item label={'Default Button'}>
-                <KButton {...args} large label={'안녕하세요'}/>
-                <KButton large label={'Hello'}/>
-                <KButton medium label={'こんにちは'}/>
-                <KButton small label={'Dev'}/>
-                <KButton large disabled label={'Disabled'}/>
-            </Item>
-            <Item label={'Primary Button'}>
-                <KButton primary large label={'안녕하세요'}/>
-                <KButton primary large label={'Large'}/>
-                <KButton primary medium label={'Medium'}/>
-                <KButton primary small label={'Small'}/>
-                <KButton primary large disabled label={'Disabled'}/>
-            </Item>
-            <Item label={'Outlined Button'}>
-                <KButton outlined large label={'안녕하세요'}/>
-                <KButton outlined large label={'Large'}/>
-                <KButton outlined medium label={'Medium'}/>
-                <KButton outlined small label={'Small'}/>
-                <KButton outlined large disabled label={'Disabled'}/>
-            </Item>
-            <Item label={'Custom Color Button'}>
-                <KButton primary large label={'안녕하세요'} color={'#FF9B9B'}/>
-                <KButton primary large label={'Large'} color={'#FF9B9B'}/>
-                <KButton primary medium label={'Medium'} color={'#FFD6A5'}/>
-                <KButton primary small label={'Small'} color={'#CBFFA9'}/>
-                <KButton primary large label={'Disabled'} disabled color={'#FF9B9B'}/>
-            </Item>
-            <Item label={'Custom Outline Color Button'}>
-                <KButton large label={'안녕하세요'} color={'#7B66FF'}/>
-                <KButton large label={'Large'} color={'#7B66FF'}/>
-                <KButton medium label={'Medium'} color={'#5FBDFF'}/>
-                <KButton small label={'Small'} color={'#96EFFF'}/>
-                <KButton large label={'Disabled'} disabled color={'#7B66FF'}/>
-            </Item>
-            <Item label={'Loading Button'}>
-                <KButton ref={loadButtonRef1} large label={'Loading'}/>
-                <KButton ref={loadButtonRef2} outlined large label={'Loading'}/>
-                <KButton ref={loadButtonRef3} primary large label={'Loading'} color={'#5FBDFF'}/>
-                <KButton ref={loadButtonRef4} primary medium color={'#7B66FF'} label={'Loading'}/>
-                <KButton ref={loadButtonRef5} primary color={'#FF9B9B'} small label={'Loading'}/>
-            </Item>
-        </Container>
-    );
+    return (<KButton {...args}/>);
 };
 
 export const Default: Story = {
     render: Template,
-    args: {},
+    args: {
+        label: '안녕하세요',
+        variant: 'default',
+        size: 'medium',
+    },
 };
 
---
+++ stories/components/button/KButton.variant.stories.tsx
@@ -0,0 +1,50 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {Container, Item} from '../common/Container';
+import {KButton} from '@/components';
+import {KButtonProps} from '@/components/button/KButton.interface';
+
+const meta: Meta<KButtonProps> = {title: 'Button', component: KButton};
+
+export default meta;
+
+type Story = StoryObj<typeof KButton>
+
+const Template = (args: KButtonProps) => {
+
+
+    return (
+        <Container>
+            <Item label={'Default'}>
+                <KButton {...args} label={'안녕하세요'}/>
+                <KButton label={'Hello'}/>
+                <KButton label={'こんにちは'}/>
+            </Item>
+            <Item label={'Primary'}>
+                <KButton primary label={'안녕하세요'}/>
+                <KButton primary label={'Hello'}/>
+                <KButton primary label={'こんにちは'}/>
+            </Item>
+            <Item label={'Outlined'}>
+                <KButton outlined label={'안녕하세요'}/>
+                <KButton outlined label={'Hello'}/>
+                <KButton outlined label={'こんにちは'}/>
+            </Item>
+            <Item label={'Custom Color'}>
+                <KButton primary label={'안녕하세요'} color={'#FF9B9B'}/>
+                <KButton primary label={'Hello'} color={'#CBFFA9'}/>
+                <KButton primary label={'こんにちは'} color={'#FFD6A5'}/>
+            </Item>
+            <Item label={'Custom Outline Color'}>
+                <KButton label={'안녕하세요'} color={'#7B66FF'}/>
+                <KButton label={'Hello'} color={'#525CEB'}/>
+                <KButton label={'こんにちは'} color={'#5FBDFF'}/>
+            </Item>
+        </Container>
+    );
+};
+
+export const Variant: Story = {
+    render: Template,
+    args: {},
+};
+
