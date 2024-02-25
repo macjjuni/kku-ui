e86789af9bff7186872bdde019060afad6fd6780
macjjuni <macjjuni@gmail.com>
2024-02-25 17:42:36 +0900

    trivial. 스토리북 설명 파라미터 리팩토링

--- stories/components/common/argTypes.ts
+++ stories/components/common/argTypes.ts
@@ -5,12 +5,10 @@ export const variantArgType = {
 }
 
 export const sizeArgType = {
-    size: {description: '내부 여백 크기를 설정합니다.', defaultValue: {summary: 'medium'}},
-}
-export const individualSizeArgType = {
-    large: {description: '여백 크기를 Large 크기만큼 설정합니다.', defaultValue: {summary: 'false'}},
-    medium: {description: '여백 크기를 Medium 크기만큼 설정합니다.', defaultValue: {summary: 'false'}},
-    small: {description: '여백 크기를 Small 크기만큼 설정합니다.', defaultValue: {summary: 'false'}},
+    size: {description: '요소의 크기를 설정합니다.', defaultValue: {summary: 'medium'}},
+    large: {description: '요소의 크기를 Large 상태로 설정합니다.', defaultValue: {summary: 'false'}},
+    medium: {description: '요소의 크기를 Medium 상태로 설정합니다.', defaultValue: {summary: 'false'}},
+    small: {description: '요소의 크기를 Small 상태로 설정합니다.', defaultValue: {summary: 'false'}},
 }
 
 export const disabledArgType = {
@@ -21,7 +19,7 @@ export const colorArgType = {
     color: {description: '색상을 설정합니다.', defaultValue: { summary: 'undefined' }},
 }
 
-export const commonArgType = {
+export const baseArgTyp = {
     id: {description: 'id 속성 값을 설정합니다.', defaultValue: { summary: 'undefined' }},
     className: {description: 'class 속성 값을 설정합니다.', defaultValue: { summary: 'undefined' }},
     style: {description: 'style 속성 값을 설정합니다.', defaultValue: { summary: 'undefined' }},
