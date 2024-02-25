efb6ea39d7b3508f4dbcb9f8e3fa299221e817d9
macjjuni <macjjuni@gmail.com>
2024-02-25 17:43:18 +0900

    feature. 스토리북 개편

--- stories/components/button/KButton.default.stories.tsx
+++ stories/components/button/KButton.default.stories.tsx
@@ -1,15 +1,7 @@
 import type {Meta, StoryObj} from '@storybook/react';
 import {KButton} from '@/components';
 import {KButtonProps} from '@/components/button/KButton.interface';
-import {
-    commonArgType,
-    sizeArgType,
-    disabledArgType,
-    colorArgType,
-    variantArgType,
-    individualSizeArgType,
-    onClickArgType,
-} from '../common/argTypes';
+import {baseArgTyp, sizeArgType, disabledArgType, colorArgType, variantArgType, onClickArgType} from '../common/argTypes';
 
 const meta: Meta<KButtonProps> = {
     component: KButton,
@@ -18,13 +10,8 @@ const meta: Meta<KButtonProps> = {
         label: {description: '내부 텍스트를 설정합니다.'},
         primary: {description: '버튼 색상 형태를 설정합니다.', defaultValue: {summary: 'false'}},
         outlined: {description: '버튼 색상을 외곽선 형태로 설정합니다.', defaultValue: {summary: 'false'}},
-        ...onClickArgType,
-        ...sizeArgType,
-        ...disabledArgType,
-        ...colorArgType,
-        ...variantArgType,
-        ...commonArgType,
-        ...individualSizeArgType
+        ...onClickArgType, ...sizeArgType, ...disabledArgType,
+        ...colorArgType, ...variantArgType, ...baseArgTyp,
     },
 };
 
