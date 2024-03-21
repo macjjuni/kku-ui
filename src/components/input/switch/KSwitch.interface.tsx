d036a475255da1a65bb50d82f7bb9414ba1355d1
macjjuni <macjjuni@gmail.com>
2024-03-21 23:52:22 +0900

    feature. switch 컴포넌트 개발 #1. switch 초기화

---
+++ src/components/input/switch/KSwitch.interface.tsx
@@ -0,0 +1,22 @@
+import { KBaseProp, KSizeProp } from '@/common/base/base.interface';
+
+
+export interface KSwitchProps extends KBaseProp, KSizeProp {
+
+  value: boolean // ✨ Required ✨
+
+  // Types
+  color?: string
+
+  disabled?: boolean
+
+  // Event
+  onChange: (value: boolean) => void // ✨ Required ✨
+}
+
+
+export interface KSwitchRefs {
+  on: () => void
+  off: () => void
+  toggle: () => void
+}
---
+++ src/components/input/switch/KSwitch.tsx
@@ -0,0 +1,100 @@
+import { CSSProperties, forwardRef, memo, Ref, useCallback, useImperativeHandle, useMemo, useRef } from 'react';
+import { initDisabled, initSize } from '@/common/util/variation';
+import { KSwitchProps, KSwitchRefs } from '@/components/input/switch/KSwitch.interface';
+
+const KSwitch = forwardRef((props: KSwitchProps, ref: Ref<KSwitchRefs>) => {
+
+  // region [Hooks]
+
+  const inputRef = useRef<HTMLButtonElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    toggle() { inputRef.current?.click(); },
+    on() {},
+    off() {},
+  }));
+
+  // endregion
+
+
+  // region [Privates]
+  // endregion
+
+
+  // region [Styles]
+
+  const rootClass = useMemo(() => {
+
+    const clazz = [];
+
+    if (props.className) { clazz.push(props.className); }
+
+    initSize(clazz, 'k-switch', props.size, props.large, props.medium, props.small);
+    initDisabled(clazz, 'k-switch', props.disabled);
+
+    return clazz.join(' ');
+  }, [
+    props.className, props.size, props.small, props.medium, props.large, props.disabled]);
+
+  const rootStyle = useMemo(() => {
+
+    const style: CSSProperties = { ...props.style };
+
+    return style;
+  }, [props.style]);
+
+  const toggleClass = useMemo(() => {
+
+    if (props.value) { return 'k-switch__on'; }
+
+    return 'k-switch__off';
+  }, []);
+
+  // endregion
+
+
+  // region [Events]
+
+  const onChange = useCallback(() => {
+
+    props.onChange(!props.value);
+  }, [props.onChange]);
+
+  // endregion
+
+
+  // region [Life Cycles]
+
+  // endregion
+
+
+  // region [Template]
+
+  // endregion
+
+
+  return (
+  // eslint-disable-next-line react/button-has-type
+    <button
+            ref={inputRef}
+            id={props.id}
+            role='switch'
+            style={rootStyle}
+            aria-checked={props.value}
+            aria-label='switch'
+            disabled={props.disabled}
+            tabIndex={!props.disabled ? 0 : -1}
+            className={`k-switch ${rootClass}`}
+            onChange={onChange}
+            data-testid='k-swtich'
+    >
+      <span className={`k-switch__toggle ${toggleClass}`} />
+    </button>
+  );
+});
+
+
+KSwitch.defaultProps = {};
+KSwitch.displayName = 'KSwitch';
+
+export default memo(KSwitch);
