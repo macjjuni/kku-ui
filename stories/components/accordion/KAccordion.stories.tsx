e5889cb5826f3c2c3367ee9933675de549fb3675
macjjuni <macjjuni@gmail.com>
2024-04-27 21:02:16 +0900

    feature. 아코디언 컴포넌트 스토리북, 테스트 코드 작성

---
+++ stories/components/accordion/KAccordion.stories.tsx
@@ -0,0 +1,46 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {KAccordionProps, KAccordion} from '@/components';
+
+const meta: Meta<KAccordionProps> = {
+    component: KAccordion,
+    title: 'Components/Accordion',
+    argTypes: {
+        size: {description: '크기를 설정합니다.', defaultValue: {summary: 'medium'}},
+        id: {description: 'id 속성을 설정합니다.', defaultValue: {summary: 'undefined'}},
+        className: {description: 'class 속성을 설정합니다.', defaultValue: {summary: 'undefined'}},
+        style: {description: 'style 속성을 설정합니다.', defaultValue: {summary: 'undefined'}},
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof KAccordion>
+
+const Template = (args: KAccordionProps) => {
+
+
+    return (
+        <>
+            <KAccordion {...args} open large/>
+            <br/><br/><br/>
+            <KAccordion {...args} medium/>
+            <br/><br/><br/>
+            <KAccordion {...args} small/>
+        </>
+    );
+};
+
+export const Default: Story = {
+    render: Template,
+    args: {
+        summary: 'Lorem Ipsum',
+        children: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
+            'Lorem Ipsum has been the industry standard dummy text ever since the 1500s, ' +
+            'when an unknown printer took a galley of type and scrambled it to make a type ' +
+            'specimen book. It has survived not only five centuries, but also the leap into electronic' +
+            ' typesetting, remaining essentially unchanged. It was popularised in the 1960s with ' +
+            'the release of Letraset sheets containing Lorem Ipsum passages, and more recently with ' +
+            'desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    },
+};
+
