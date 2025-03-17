46033f051787896fa1b0ca3316f11a8a71f1d8ca
macjjuni <macjjuni@gmail.com>
2025-03-17 14:14:45 +0900

    feature. jest -> Vitest 마이그레이션 #1

---
+++ setupTests.ts
@@ -0,0 +1,1 @@
+import '@testing-library/jest-dom'; // Jest DOM 매처 추가
---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config';
+import path from 'path'; // ⬅️ 추가
+import viteConfig from './vite.config';
+
+
+export default defineConfig({
+  ...viteConfig,
+  test: {
+    globals: true, // Jest처럼 `globalThis`를 사용할 수 있도록 설정
+    environment: 'jsdom', // React 컴포넌트 테스트를 위한 JSDOM 환경
+    setupFiles: './setupTests.ts', // 테스트 실행 전에 필요한 설정 파일
+    coverage: {
+      provider: 'v8', // 코드 커버리지 제공자 설정 (v8 또는 istanbul)
+      reporter: ['text', 'lcov'], // 커버리지 리포트 형식
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'), // ⬅️ alias 추가
+      },
+    },
+  },
+});
