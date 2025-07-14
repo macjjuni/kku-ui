2a874a028b7813874ef22da732506b0ff8bc62dc
macjjuni <macjjuni@gmail.com>
2025-07-14 10:24:52 +0900

    chore. 스토리북 배포 에러 수정 #9

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,41 +1,36 @@
 import { mergeConfig } from 'vite';
-import path from 'path';
 
 export default {
-    stories: ['../**/*.stories.@(ts|tsx|js|jsx)'],
-    core: { builder: '@storybook/builder-vite' },
-    addons: [
-        '@storybook/addon-a11y',
-        '@storybook/addon-links',
-        '@chromatic-com/storybook',
-        'storybook-dark-mode',
-        '@storybook/addon-docs',
-    ],
-    framework: { name: '@storybook/react-vite', options: {} },
-    docs: {},
-    async viteFinal(config: any) {
-        return mergeConfig(config, {
-            cacheDir: './.vite-cache',
-            optimizeDeps: {
-                force: true,
-                include: ['react', 'react-dom', '@storybook/react', '@storybook/addon-docs', '@storybook/addon-a11y'],
-            },
-            resolve: {
-                alias: {
-                    '@sb-common-assets': path.resolve(__dirname, '../public/fonts'),
-                },
-            },
-            build: {
-                rollupOptions: { external: [] },
-            },
-        });
+  staticDirs: ['../sb-common-assets'],
+  stories: ['../**/*.stories.@(ts|tsx|js|jsx)'],
+  core: { builder: '@storybook/builder-vite' },
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@chromatic-com/storybook',
+    'storybook-dark-mode',
+    '@storybook/addon-docs',
+  ],
+  framework: { name: '@storybook/react-vite', options: {} },
+  docs: {},
+  async viteFinal(config: any) {
+    return mergeConfig(config, {
+      cacheDir: './.vite-cache',
+      optimizeDeps: {
+        force: true,
+        include: ['react', 'react-dom', '@storybook/react', '@storybook/addon-docs', '@storybook/addon-a11y'],
+      },
+      build: {
+        rollupOptions: { external: [] },
+      },
+    });
+  },
+  typescript: {
+    check: true,
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      shouldRemoveUndefinedFromOptional: true,
     },
-    typescript: {
-        check: true,
-        reactDocgen: 'react-docgen-typescript',
-        reactDocgenTypescriptOptions: {
-            shouldExtractLiteralValuesFromEnum: true,
-            shouldRemoveUndefinedFromOptional: true,
-        },
-    },
+  },
 };
