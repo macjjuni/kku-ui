b9d0a853280149ecfabfcb981a82b40581b4a0d9
macjjuni <macjjuni@gmail.com>
2025-07-14 09:47:49 +0900

    chore. 스토리북 배포 에러 수정 #1

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
     stories: ['../**/*.stories.@(ts|tsx|js|jsx)'],
@@ -12,18 +13,18 @@ const config: StorybookConfig = {
         'storybook-dark-mode',
         '@storybook/addon-docs',
     ],
-
     framework: {
         name: '@storybook/react-vite',
         options: {},
     },
-
     docs: {},
-
     async viteFinal(config) {
-        return config;
+        return mergeConfig(config, {
+            optimizeDeps: {
+                include: ['@storybook/react', '@storybook/addon-docs', '@storybook/addon-a11y'],
+            },
+        });
     },
-
     typescript: {
         check: true,
         reactDocgen: 'react-docgen-typescript',
