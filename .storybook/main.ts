1270f2ed218d8f939d895e0ab6a61e72bbf7e76e
macjjuni <macjjuni@gmail.com>
2025-07-14 10:17:15 +0900

    chore. 스토리북 배포 에러 수정 #7

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
+import path from 'path';
 
 const config: StorybookConfig = {
     stories: ['../**/*.stories.@(ts|tsx|js|jsx)'],
@@ -25,9 +26,13 @@ const config: StorybookConfig = {
                 force: true,
                 include: ['react', 'react-dom', '@storybook/react', '@storybook/addon-docs', '@storybook/addon-a11y'],
             },
+            resolve: {
+                alias: {
+                    '@sb-common-assets': path.resolve(__dirname, '../node_modules/@storybook/core-common/dist/assets'),
+                },
+            },
             build: {
                 rollupOptions: {
-                    // Storybook에서 external 설정 무시
                     external: [],
                 },
             },
