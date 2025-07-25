55ecd7bcd102065a8e5f22a8b20ae7d79db29d57
macjjuni <macjjuni@gmail.com>
2025-07-25 20:08:57 +0900

    fix. KButton children 오류 수정

--- src/components/actions/button/KButton.tsx
+++ src/components/actions/button/KButton.tsx
@@ -9,11 +9,10 @@ const Button = forwardRef<KButtonRefs, KButtonProps>((props, ref) => {
 
   // region [Hooks]
   const {
-    type = 'button', className, label, disabled,
-    isDanger, size = 'medium', variant = 'outlined',
-    children, width, height, style,
-    onClick, onMouseDown, onMouseLeave, onMouseUp,
-    onKeyDown, onKeyUp, ...restProps
+    type = 'button', className, label, disabled, isDanger,
+    size = 'medium', variant = 'outlined', width, height, style,
+    onClick, onMouseDown, onMouseLeave, onMouseUp, onKeyDown, onKeyUp,
+    ...restProps
   } = props;
 
   const rootRef = useRef<HTMLButtonElement>(null);
