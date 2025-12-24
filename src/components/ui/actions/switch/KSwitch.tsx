e121d7fb4b5e52a2fbdb097c70b1efe0828f93cc
macjjuni <macjjuni@gmail.com>
2025-12-24 14:56:34 +0900

    feat. KSwitch 디자인 수정

--- src/components/ui/actions/switch/KSwitch.tsx
+++ src/components/ui/actions/switch/KSwitch.tsx
@@ -2,8 +2,7 @@ import { ComponentRef, ComponentPropsWithoutRef, forwardRef } from 'react';
 import * as SwitchPrimitives from '@radix-ui/react-switch';
 import { cn } from '@/lib/utils';
 
-export interface KSwitchProps extends ComponentPropsWithoutRef<typeof SwitchPrimitives.Root> {
-}
+export interface KSwitchProps extends ComponentPropsWithoutRef<typeof SwitchPrimitives.Root> {}
 
 const KSwitch = forwardRef<ComponentRef<typeof SwitchPrimitives.Root>, KSwitchProps>(
   ({ className, ...props }, ref) => (
@@ -12,6 +11,8 @@ const KSwitch = forwardRef<ComponentRef<typeof SwitchPrimitives.Root>, KSwitchPr
       className={cn(
         'k-switch peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors',
         'disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
+        "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring data-[state=checked]:focus-visible:ring-primary " +
+        "focus-visible:ring-offset-2 focus-visible:ring-offset-background",
         className,
       )}
       {...props}
